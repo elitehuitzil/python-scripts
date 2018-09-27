7cc65c138f4e43fb6d7b4f299565723a35a63f52
maginante <elitehuitzil@gmail.com>
2018-09-27 13:48:50 -0500

    Comentarios
    Añadi comentarios y arreglo del digito verificador cuando resultaba <9.

--- referencias_santander.py
+++ referencias_santander.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
 #
-#   elitehuitzil@gmail.com
+#   coder: elitehuitzil@gmail.com
 #   
 #       BANCO SANTANDER MÉXICO
 #       ALGORITMO No. 22 Módulo 22 numérico Año Base 2009
+#
 #   Parámetros requeridos para el cálculo:
 #   Tipo de Referencia : Numérica (N)
 #   Longitud Mínima de la Referencia : 20
@@ -17,14 +18,21 @@
 ###############################################################################
 
 import datetime
-
+# referencia interna, puede ser folio de factura, matricula, numerod e cliente, etc.
 referencia = '123016102016'
+# el monto que debe paga el cliente.
 importe = '4918.00'
+# la fecha límite de pago para el cliente
 fecha_vencimiento = '11-06-2017'
+
+# estos datos son propios del algoritmo 22 de Santander.
+ano = 2009
 divisor = 10
 divisor2 = 97
 factorfijo = 1
 constante = 2
+
+# algoritmo...
 fecha_condensada = 0
 importe_condensado = 0
 
@@ -32,7 +40,7 @@ date = datetime.datetime.strptime(fecha_vencimiento, "%d-%m-%Y")
 dia_vencimiento = date.day
 mes_vencimiento = date.month
 ano_vencimiento = date.year
-ano_condensado = (ano_vencimiento - 2009) * 372
+ano_condensado = (ano_vencimiento - ano) * 372
 mes_condensado = (mes_vencimiento - 1) * 31
 dia_condensado = dia_vencimiento - 1
 fecha_condensada = dia_condensado + mes_condensado + ano_condensado
@@ -65,11 +73,14 @@ for numero in referencia:
         inter2 += 1
 digito_verificador = (suma - (divisor2 * (suma / divisor2))) + factorfijo
 
+if len(str(digito_verificador)) == 1:
+            digito_verificador = '0' + str(digito_verificador)
+
 referencia_bancaria = referencia_str + str(digito_verificador)
 
-
+# formato XXXX XXXX XXXX XXXX XXXX
 def encrypt(string, length):
     return ' '.join(string[i:i + length] for i in xrange(0, len(string), length))
 
-
+# print :)
 print "referencia = %s" % encrypt(referencia_bancaria, 4)
